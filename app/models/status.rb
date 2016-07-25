c09bf4fdf7ec6fb80a2e92ee6b71d502b5a8d79e
Adrian Setyadi <mail@felicitrans.com>
2016-07-25 07:11:25 +0700

    Clean status.rb

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,13 +1,10 @@
 require 'koala'
 
 class Status < ApplicationRecord
-  def self.search current_user, how_many, query, options = {result_type: 'recent'}
+  def self.search current_user, how_many, query
     Koala.config.api_version = "v2.0"
     @@client = Koala::Facebook::API.new(current_user.oauth_token)
     profile = @@client.get_object("me")
     friends = @@client.get_connections("me", "friends")
-    # @@statuses = @@client.search(query, options).take(how_many).collect do |tweet|
-    #   tweet
-    # end
   end
 end
