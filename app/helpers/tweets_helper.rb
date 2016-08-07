237c126693b122def8740e84ac40779f240ddd60
Adrian Setyadi <mail@felicitrans.com>
2016-08-07 20:01:48 +0700

    Move linking to user profile to linkify method

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,14 +4,14 @@ module TweetsHelper
   def linkify html
     html.gsub(%r{https?://[^\s<]+}) do |url|
       "<a href='#{url}' rel='nofollow' target='_blank'>#{url}</a>"
+    end.gsub(%r{\@\w+}) do |sn|
+      sn[0] = ''
+      "<a href='https://twitter.com/#{sn}'>@#{sn}</a>"
     end
   end
 
   def color_hashtag html
-    html.gsub(%r{\@\w+}) do |sn|
-      sn[0] = ''
-      "<a href='https://twitter.com/#{sn}'>@#{sn}</a>"
-    end.gsub(%r{(?<=\s|\b|^)\#\w+(?=\b)}) do |h|
+    html.gsub(%r{(?<=\s|\b|^)\#\w+(?=\b)}) do |h|
       "<span class='hashtag'>#{h}</span>"
     end
   end
